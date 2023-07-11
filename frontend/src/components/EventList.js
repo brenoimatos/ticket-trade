0ef545f40615f35ebf8892576280d11605061ccc
Breno Matos <breno.imbico@gmail.com>
2023-07-11 11:55:32 -0300

    eventList and eventSearch responsive with MUI

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Typography, Box } from '@mui/material'
+import { styled } from '@mui/system'
 
-function EventList({ events }) {
+const EventCard = styled(Box)(({ theme }) => ({
+  backgroundColor: '#f9f9f9',
+  border: '1px solid #ddd',
+  padding: theme.spacing(1.5),
+  margin: theme.spacing(0.4, 0),
+  borderRadius: '5px',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  '&:hover': {
+    backgroundColor: '#f1f1f1',
+  },
+}))
+
+const EventList = ({ events }) => {
   const formatDate = (dateString) => {
     const options = {
       weekday: 'long',
@@ -13,24 +30,59 @@ function EventList({ events }) {
   }
 
   return (
-    <div className="event-list">
+    <Box className="event-list" sx={{ width: '100%', m: '0', pl: '0px' }}>
       {events.map((event) => (
         <Link
           key={event.id}
           to={`/events/${event.id}/tickets`}
           className="event-link"
         >
-          <div className="event-card">
-            <div className="event-date">
-              <span className="date-icon">📅</span>
-              <span>{formatDate(event.date)}</span>
-            </div>
-            <h4>{event.name}</h4>
-            <h5 className="event-location">📍 {event.location}</h5>
-          </div>
+          <EventCard sx={{ pl: '0px' }}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                marginBottom: '3px',
+                color: 'red',
+                fontSize: '1rem',
+                pl: '10px',
+              }}
+            >
+              <span role="img" aria-label="date-icon">
+                📅{' '}
+              </span>
+              {formatDate(event.date)}
+            </Typography>
+            <Typography
+              variant="h4"
+              component="h2"
+              sx={{
+                marginBottom: '3px',
+                fontWeight: 'bold',
+                fontSize: '18px',
+                lineHeight: '18px',
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                pl: '10px',
+              }}
+            >
+              {event.name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              className="event-location"
+              sx={{ fontSize: '14px', color: '#333', pl: '10px' }}
+            >
+              <span role="img" aria-label="location-icon">
+                📍{' '}
+              </span>
+              {event.location}
+            </Typography>
+          </EventCard>
         </Link>
       ))}
-    </div>
+    </Box>
   )
 }
 
--- frontend/src/components/EventSearch.js
+++ frontend/src/components/EventSearch.js
@@ -1,54 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { apiBaseUrl } from '../api/apiConfig';
-import EventList from './EventList';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from 'react'
+import { apiBaseUrl } from '../api/apiConfig'
+import EventList from './EventList'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Box, InputBase, IconButton, Container } from '@mui/material'
 
 function EventSearch() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
 
   useEffect(() => {
     const delayTimer = setTimeout(() => {
-      setDebouncedSearchTerm(searchTerm);
-    }, 300);
+      setDebouncedSearchTerm(searchTerm)
+    }, 300)
 
-    return () => clearTimeout(delayTimer);
-  }, [searchTerm]);
+    return () => clearTimeout(delayTimer)
+  }, [searchTerm])
 
   useEffect(() => {
     if (debouncedSearchTerm.length >= 2 || debouncedSearchTerm.length === 0) {
-      fetch(`${apiBaseUrl}/events/?search=${debouncedSearchTerm}&sort=["name", "ASC"]&range=[0, 5]`)
-        .then(res => res.json())
-        .then(data => setSearchResults(data))
-        .catch(err => console.error(err));
+      fetch(
+        `${apiBaseUrl}/events/?search=${debouncedSearchTerm}&sort=["name", "ASC"]&range=[0, 5]`
+      )
+        .then((res) => res.json())
+        .then((data) => setSearchResults(data))
+        .catch((err) => console.error(err))
     } else {
-      setSearchResults([]);
+      setSearchResults([])
     }
-  }, [debouncedSearchTerm]);
+  }, [debouncedSearchTerm])
 
   const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
+    setSearchTerm(event.target.value)
+  }
 
   return (
-    <div className="search-container">
-      <div className="search-bar">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Pesquisar eventos"
-          className="search-input"
-        />
-        <span className="search-icon">
+    <Container sx={{ width: { sm: '70%', xs: '90%' } }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100%',
+          marginTop: 1,
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            width: { sm: '80%', xs: '90%' },
+            marginTop: 2,
+            borderRadius: '20px',
+            border: '2px solid #ccc',
+            position: 'relative',
+          }}
+        >
+          <InputBase
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Pesquisar eventos"
+            sx={{
+              height: '40px',
+              paddingLeft: 3,
+              paddingRight: 1,
+              border: 'none',
+              fontSize: '16px',
+              outline: 'none',
+              width: '100%',
+            }}
+          />
+          <IconButton size="small" sx={{ marginRight: '10px' }}>
             <FontAwesomeIcon icon={faSearch} />
-        </span>
-      </div>
-      {searchResults && <EventList events={searchResults} />}
-    </div>
-  );
+          </IconButton>
+        </Box>
+        {searchResults && (
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <EventList events={searchResults} />
+          </Box>
+        )}
+      </Box>
+    </Container>
+  )
 }
 
-export default EventSearch;
+export default EventSearch
